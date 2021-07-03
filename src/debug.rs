54c661f87da21c7e6d999a40de646fabd27ecb2c
Daemitrious <evandotschwartzentruber@gmail.com>
2021-07-03 14:32:51 -0400

    fixed

---
+++ src/debug.rs
@@ -0,0 +1,23 @@
+use {rand::Rng, std::ops::Range};
+
+//  TP
+const ASCII: [char; 94] = [
+    '@', '`', '!', 'A', 'a', '"', 'B', 'b', '#', 'C', 'c', '$', 'D', 'd', '%', 'E', 'e', '&', 'F',
+    'f', '\'', 'G', 'g', '(', 'H', 'h', ')', 'I', 'i', '*', 'J', 'j', '+', 'K', 'k', ',', 'L', 'l',
+    '-', 'M', 'm', '.', 'N', 'n', '/', 'O', 'o', '0', 'P', 'p', '1', 'Q', 'q', '2', 'R', 'r', '3',
+    'S', 's', '4', 'T', 't', '5', 'U', 'u', '6', 'V', 'v', '7', 'W', 'w', '8', 'X', 'x', '9', 'Y',
+    'y', ':', 'Z', 'z', ';', '[', '{', '<', '\\', '|', '=', ']', '}', '>', '^', '~', '?', '_',
+];
+
+//  TP
+fn randint(range: Range<usize>) -> usize {
+    rand::thread_rng().gen_range(range)
+}
+//  TP
+pub fn randstr(size: Range<usize>) -> String {
+    let mut string = "".to_owned();
+    for _ in 0..randint(size) {
+        string.push_str(&ASCII[randint(0..93)].to_string())
+    }
+    string
+}
---
+++ src/main.rs
@@ -0,0 +1,118 @@
+//  Debugging
+mod debug;
+use debug::randstr;
+use std::time::SystemTime;
+
+//  Using the `try_into` method |   &[u8] -> [u8; 4]
+use std::convert::TryFrom;
+
+//  Used to determine size of main vector and downscale hash values generated from `Account::hash`
+const MIN: u32 = 555_819_297;
+const MAX: u32 = 2_122_219_134;
+
+//  Greatest Prime number of the value 1,000 lesser than `MIN`
+const PRIME: u32 = 555_767;
+
+trait Hashable {
+    fn hash(&self) -> usize;
+}
+impl Hashable for String {
+    fn hash(&self) -> usize {
+        ((u32::from_ne_bytes(TryFrom::try_from(self[..4].as_bytes()).unwrap()) - MIN) / PRIME)
+            as usize
+    }
+}
+
+#[derive(Debug)]
+struct Account {
+    user: String,
+    pass: String,
+}
+impl Account {
+    fn new(user: &str, pass: &str) -> Option<Self> {
+        if user.is_ascii()
+            && pass.is_ascii()
+            && user.len() > 0
+            && pass.len() > 7
+            && user.len() < 15
+            && pass.len() < 30
+        {
+            Some(Account {
+                user: user.to_string(),
+                pass: pass.to_string(),
+            })
+        } else {
+            None
+        }
+    }
+    fn random() -> Self {
+        Account {
+            user: randstr(1..15),
+            pass: randstr(8..30),
+        }
+    }
+}
+
+struct Database(Vec<Vec<Account>>);
+
+impl Database {
+    fn add(&mut self, account: Account) -> Result<(), Account> {
+        if account.pass.hash() == 4294967295 {
+            return Err(account);
+        }
+        Ok(self.0[account.pass.hash()].push(account))
+    }
+    fn find(&self, user: String, pass: String) -> Option<&Account> {
+        let row = &self.0[pass.hash()];
+        if row.len() > 0 {
+            for account in row.iter() {
+                if user == account.user && pass == account.pass {
+                    return Some(account);
+                }
+            }
+            None
+        } else {
+            None
+        }
+    }
+    fn new() -> Self {
+        Self((0..((MAX - MIN) / PRIME) + 1).map(|_| Vec::new()).collect())
+    }
+}
+
+fn main() {
+    let mut data = Database::new();
+
+    //  Generate a ton of random accounts
+    for _ in 0..10_000_000 {
+        if let Err(account) = data.add(Account::random()) {
+            println!("{:?}", account)
+        }
+    }
+    //  Add a specific account
+    let test = Account::new("OwenIsGay", "SuhjinTheFuck69").unwrap();
+    drop(data.add(Account::new("OwenIsGay", "SuhjinTheFuck69").unwrap()));
+
+    //  Benchmark how long it takes to find that specific account
+    let a = SystemTime::now();
+    let option = data.find(test.user, test.pass);
+    let b = SystemTime::now();
+
+    //  Checks if the find was successful
+    match option {
+        Some(user) => println!("{:?}", user),
+        _ => println!("None"),
+    }
+
+    //  How long it took to find the account
+    println!("{:?}\n", b.duration_since(a));
+
+    //  The total amount of accounts inside of the database
+    println!("Total amount of accounts = {}", {
+        let mut total = 0;
+        for row in data.0.iter() {
+            total += row.len()
+        }
+        total
+    })
+}
