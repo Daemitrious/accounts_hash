238a2643521ddda6c7d00a1b9fcc0e06e3077dbd
Daemitrious <evandotschwartzentruber@gmail.com>
2021-07-17 01:43:32 -0400

    update

--- src/debug.rs
+++ src/debug.rs
@@ -1,6 +1,8 @@
-use {rand::Rng, std::ops::Range};
+use {
+    rand::Rng,
+    std::{ops::Range, time::SystemTime},
+};
 
-//  TP
 const ASCII: [char; 94] = [
     '@', '`', '!', 'A', 'a', '"', 'B', 'b', '#', 'C', 'c', '$', 'D', 'd', '%', 'E', 'e', '&', 'F',
     'f', '\'', 'G', 'g', '(', 'H', 'h', ')', 'I', 'i', '*', 'J', 'j', '+', 'K', 'k', ',', 'L', 'l',
@@ -9,11 +11,10 @@ const ASCII: [char; 94] = [
     'y', ':', 'Z', 'z', ';', '[', '{', '<', '\\', '|', '=', ']', '}', '>', '^', '~', '?', '_',
 ];
 
-//  TP
 pub fn randint(range: Range<usize>) -> usize {
     rand::thread_rng().gen_range(range)
 }
-//  TP
+
 pub fn randstr(size: Range<usize>) -> String {
     let mut string = "".to_owned();
     for _ in 0..randint(size) {
@@ -21,3 +22,88 @@ pub fn randstr(size: Range<usize>) -> String {
     }
     string
 }
+
+//  const FIND: &str = "find";
+
+/*
+    //  The total amount of random accounts to be generated
+    let amount = 1_000_000;
+
+    println!("Generating {} accounts...", amount);
+
+    for _ in 0..amount {
+        loop {
+            let (user, pass) = (&randstr(4..15), &randstr(8..25));
+            match data.add(user, pass).await {
+                Err(why) => println!("{} => \"{}\"", why.to_string(), user),
+                _ => break,
+            }
+        }
+    }
+    //  The testing account's ("TA") username and password
+    let (user, pass) = ("John", "EatMyWhale69");
+
+    //  Add the TA to the filled database
+    drop(data.add(user, pass).await);
+
+    //  Hash method
+    let a1 = SystemTime::now();
+    let f1 = data.find(user, pass).await;
+    let b1 = SystemTime::now();
+
+    //  Iteration method
+    let a2 = SystemTime::now();
+    let f2 = data.normal(user, pass).await;
+    let b2 = SystemTime::now();
+
+    let t1 = b1.duration_since(a1).unwrap();
+    let t2 = b2.duration_since(a2).unwrap();
+
+    //  Checks if the find was successful
+    println!(
+        "\nHash Method === {} === [{:?}]\nFor Loop === {} === [{:?}]\n\nHash over for loop :: {:.2}\nTotal amount of accounts :: {}\n",
+        if let Some(_) = match f1 {
+            Ok(option) => option,
+            Err(why) => {
+                println!("{}", why.to_string());
+                None
+            }
+        } { "Pass" } else { "Fail" },
+        t1,
+        if let Some(_) = match f2 {
+            Ok(option) => option,
+            Err(why) => {
+                println!("{}", why.to_string());
+                None
+            }
+        } { "Pass" } else { "Fail" },
+        t2,
+        t2.as_secs_f64() / t1.as_secs_f64(),
+
+        {
+            let mut total = 0;
+            for row in data.0.iter() {
+                total += row.len()
+            }
+            total
+        }
+    );
+*/
+
+/*
+    //  Search using a basic `for` loop
+    async fn normal(&self, user: &str, pass: &str) -> Result<Option<&Account>, Reason> {
+        if let Err(why) = check(user, pass).await {
+            Err(why)
+        } else {
+            for x in self.0.iter() {
+                for account in x.iter() {
+                    if account.user == user && account.pass == pass {
+                        return Ok(Some(account));
+                    }
+                }
+            }
+            Ok(None)
+        }
+    }
+*/
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,18 @@
-//  Debugging   ////////////
-//mod debug;
-
-//use debug::randstr;
-use std::time::SystemTime;
-////////////////////////////
-
 use {
-    serde::{
-        ser::{SerializeStruct, SerializeTupleStruct},
-        Serialize,
+    serde::{Deserialize, Serialize},
+    serde_json::{from_reader, to_writer, Error as sj_Error},
+    std::{
+        convert::TryFrom,
+        fs::File,
+        io::{stdin, BufReader, Error as io_Error},
     },
-    serde_json::{to_writer, Error},
-    std::{convert::TryFrom, fs::File, io::stdin},
-    Reason::*,
 };
 
-//  Used to determine size of main vector and downscale hash values generated from `Account::hash`
+//  Lowest and highest possible hash values from `String::hash()`
 const MIN: u32 = 555_819_297;
 const MAX: u32 = 2_122_219_134;
 
-//  Greatest Prime number of the value 1,000 lesser than `MIN`
+//  A prime number of the value 1/1,000 times of `MIN`
 const PRIME: u32 = 555_767;
 
 //  Base Commands
@@ -30,64 +23,22 @@ const QUIT: &str = "quit";
 //  Data Commands
 const ADD: &str = "add";
 const FIND: &str = "find";
-const NORMAL: &str = "normal";
 
-async fn input() -> Result<String, ()> {
-    let mut line = String::new();
+//  Error Messages
+const ERRORS: [&str; 7] = [
+    "Username is already taken",
+    "Username is not ASCII",
+    "Username is too short",
+    "Username is too long",
+    "Password is not ASCII",
+    "Password is too short",
+    "Password is too long",
+];
 
-    match stdin().read_line(&mut line) {
-        Ok(_) => Ok(line),
-        Err(_) => Err(()),
-    }
-}
+//  Path to backup file
+const _BACKUP_PATH: &str = "accounts.json";
 
-enum Reason {
-    UserExists,
-
-    UserNotAscii,
-    UserLenLess,
-    UserLenLong,
-
-    PassNotAscii,
-    PassLenLess,
-    PassLenLong,
-}
-
-impl Reason {
-    fn to_string(&self) -> String {
-        match self {
-            UserExists => "Username is already taken",
-
-            UserNotAscii => "Username is not ASCII",
-            UserLenLess => "Username is too short",
-            UserLenLong => "Username is too long",
-
-            PassNotAscii => "Password is not ASCII",
-            PassLenLess => "Password is too short",
-            PassLenLong => "Password is too long",
-        }
-        .to_string()
-    }
-}
-
-async fn check(user: &str, pass: &str) -> Result<(), Reason> {
-    if !user.is_ascii() {
-        Err(UserNotAscii)
-    } else if !pass.is_ascii() {
-        Err(PassNotAscii)
-    } else if !(user.len() > 3) {
-        Err(UserLenLess)
-    } else if !(pass.len() > 7) {
-        Err(PassLenLess)
-    } else if !(user.len() < 15) {
-        Err(UserLenLong)
-    } else if !(pass.len() < 25) {
-        Err(PassLenLong)
-    } else {
-        Ok(())
-    }
-}
-
+//  Applies a `hash` function to `String` to conveniently grab the native endian integer value
 trait Hashable {
     fn hash(&self) -> usize;
 }
@@ -98,6 +49,33 @@ impl Hashable for String {
     }
 }
 
+//  Simple `stdin`
+async fn input() -> Result<String, io_Error> {
+    let mut line = String::new();
+    stdin().read_line(&mut line)?;
+    Ok(line)
+}
+//  Determine if `user` and `pass` are legal and compatible strings
+async fn check(user: &str, pass: &str) -> Result<(), usize> {
+    if !user.is_ascii() {
+        Err(1)
+    } else if !(user.len() > 3) {
+        Err(2)
+    } else if !(user.len() < 15) {
+        Err(3)
+    } else if !pass.is_ascii() {
+        Err(4)
+    } else if !(pass.len() > 7) {
+        Err(5)
+    } else if !(pass.len() < 25) {
+        Err(6)
+    } else {
+        Ok(())
+    }
+}
+
+//  Base account struct
+#[derive(Deserialize, Serialize, Debug)]
 struct Account {
     user: String,
     pass: String,
@@ -107,42 +85,34 @@ impl Account {
         Account { user, pass }
     }
 }
-impl Serialize for Account {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        let mut s = serializer.serialize_struct("Account", 2)?;
-        s.serialize_field("user", &self.user)?;
-        s.serialize_field("pass", &self.pass)?;
-        s.end()
-    }
-}
 
+//  Database struct
+#[derive(Deserialize, Serialize)]
 struct Database(Vec<Vec<Account>>);
 
 impl Database {
-    async fn available_username(&self, user: &str, pass: &str) -> Result<Account, Reason> {
+    //  Determine if `user` and `pass` are legal strings and `user` doesn't aleady exist
+    async fn available_username(&self, user: &str, pass: &str) -> Result<Account, usize> {
         if let Err(why) = check(user, pass).await {
             Err(why)
         } else {
             for account in self.0[user.to_string().hash()].iter() {
                 if account.user == user {
-                    return Err(UserExists);
+                    return Err(0);
                 }
             }
             Ok(Account::new(user.to_string(), pass.to_string()).await)
         }
     }
-
-    async fn add(&mut self, user: &str, pass: &str) -> Result<(), Reason> {
+    //  Push new specified account into vector of the hash value of `pass` if `available_username`
+    async fn add(&mut self, user: &str, pass: &str) -> Result<(), usize> {
         match self.available_username(&user, &pass).await {
             Ok(account) => Ok(self.0[account.user.hash()].push(account)),
             Err(why) => Err(why),
         }
     }
     //  Search method based on the hash value of the Account's `pass`
-    async fn find(&self, user: &str, pass: &str) -> Result<Option<&Account>, Reason> {
+    async fn find(&self, user: &str, pass: &str) -> Result<Option<&Account>, usize> {
         if let Err(why) = check(user, pass).await {
             Err(why)
         } else {
@@ -159,110 +129,31 @@ impl Database {
             }
         }
     }
-    //  Basic for iteration
-    async fn normal(&self, user: &str, pass: &str) -> Result<Option<&Account>, Reason> {
-        if let Err(why) = check(user, pass).await {
-            Err(why)
-        } else {
-            for x in self.0.iter() {
-                for account in x.iter() {
-                    if account.user == user && account.pass == pass {
-                        return Ok(Some(account));
-                    }
-                }
-            }
-            Ok(None)
-        }
+    //  Backup the database
+    async fn _backup(&self) -> Result<Result<(), sj_Error>, io_Error> {
+        Ok(to_writer(File::create("accounts.json")?, &self.0))
     }
+    //  Set the database to the most recent backup
+    async fn _restore(&mut self) -> Result<(), io_Error> {
+        self.0 = from_reader(BufReader::new(File::open(_BACKUP_PATH)?))?;
+        Ok(())
+    }
+    //  Build new database with preinitialized vectors
     fn new() -> Self {
         Self((0..((MAX - MIN) / PRIME) + 1).map(|_| Vec::new()).collect())
-    }
-    //  Make asyncronous at some point
-    async fn _backup(&self) -> Result<(), Error> {
-        to_writer(File::create("accounts.json").unwrap(), &self.0)
-    }
-}
-impl Serialize for Database {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        let mut ts = serializer.serialize_tuple_struct("Database", 1)?;
-        ts.serialize_field(&self.0)?;
-        ts.end()
     }
 }
 
 #[tokio::main]
 async fn main() {
     let mut data = Database::new();
-    /*
-        //  The total amount of random accounts to be generated
-        let amount = 1_000_000;
 
-        println!("Generating {} accounts...", amount);
-
-        for _ in 0..amount {
-            loop {
-                let (user, pass) = (&randstr(4..15), &randstr(8..25));
-                match data.add(user, pass).await {
-                    Err(why) => println!("{} => \"{}\"", why.to_string(), user),
-                    _ => break,
-                }
-            }
-        }
-        //  The testing account's ("TA") username and password
-        let (user, pass) = ("John", "EatMyWhale69");
-
-        //  Add the TA to the filled database
-        drop(data.add(user, pass).await);
-
-        //  Hash method
-        let a1 = SystemTime::now();
-        let f1 = data.find(user, pass).await;
-        let b1 = SystemTime::now();
-
-        //  Basic for loop
-        let a2 = SystemTime::now();
-        let f2 = data.normal(user, pass).await;
-        let b2 = SystemTime::now();
-
-        let t1 = b1.duration_since(a1).unwrap();
-        let t2 = b2.duration_since(a2).unwrap();
-
-        //  Checks if the find was successful
-        println!(
-                "\nHash Method === {} === [{:?}]\nFor Loop === {} === [{:?}]\n\nHash over for loop :: {:.2}\nTotal amount of accounts :: {}\n",
-                if let Some(_) = match f1 {
-                    Ok(option) => option,
-                    Err(why) => {
-                        println!("{}", why.to_string());
-                        None
-                    }
-                } { "Pass" } else { "Fail" },
-                t1,
-                if let Some(_) = match f2 {
-                    Ok(option) => option,
-                    Err(why) => {
-                        println!("{}", why.to_string());
-                        None
-                    }
-                } { "Pass" } else { "Fail" },
-                t2,
-                t2.as_secs_f64() / t1.as_secs_f64(),
-
-                {
-                    let mut total = 0;
-                    for row in data.0.iter() {
-                        total += row.len()
-                    }
-                    total
-                }
-            );
-    */
+    //  My first attempt at a `repl`
     loop {
         if let Ok(string) = input().await {
-            match string.trim_end() {
+            let string = string.trim_end();
+
+            match string {
                 TOTAL => {
                     let mut total = 0;
                     for v in data.0.iter() {
@@ -271,60 +162,25 @@ async fn main() {
                     println!("{}", total);
                     continue;
                 }
-                EXIT | QUIT => return,
+                EXIT | QUIT => break,
                 _ => (),
             }
 
-            let cmd = string
-                .trim_end()
-                .split_ascii_whitespace()
-                .collect::<Vec<&str>>();
+            let cmd = string.split_ascii_whitespace().collect::<Vec<&str>>();
 
             if cmd.len() == 3 {
                 match cmd[0] {
                     ADD => println!(
                         "{}",
                         match data.add(cmd[1], cmd[2]).await {
-                            Ok(_) => "Success".to_string(),
-                            Err(why) => why.to_string(),
+                            Ok(_) => "Added",
+                            Err(n) => ERRORS[n],
                         }
                     ),
-                    FIND => println!("{:?}", {
-                        let a = SystemTime::now();
-                        let result = data.find(cmd[1], cmd[2]).await;
-                        let b = SystemTime::now();
-                        (
-                            b.duration_since(a).unwrap(),
-                            match result {
-                                Ok(option) => {
-                                    if let Some(account) = option {
-                                        format!("{} {}", account.user, account.pass)
-                                    } else {
-                                        "None".to_string()
-                                    }
-                                }
-                                Err(why) => why.to_string(),
-                            },
-                        )
-                    }),
-                    NORMAL => println!("{:?}", {
-                        let a = SystemTime::now();
-                        let result = data.normal(cmd[1], cmd[2]).await;
-                        let b = SystemTime::now();
-                        (
-                            b.duration_since(a).unwrap(),
-                            match result {
-                                Ok(option) => {
-                                    if let Some(account) = option {
-                                        format!("{} {}", account.user, account.pass)
-                                    } else {
-                                        "None".to_string()
-                                    }
-                                }
-                                Err(why) => why.to_string(),
-                            },
-                        )
-                    }),
+                    FIND => match data.find(cmd[1], cmd[2]).await {
+                        Ok(option) => println!("FOUND :: {:?}", option),
+                        Err(n) => println!("{}", ERRORS[n]),
+                    },
                     _ => (),
                 }
             }
