20226d2c2ea36716ebf5a680931e3c32acaa68c2
Daemitrious <evandotschwartzentruber@gmail.com>
2021-07-19 17:15:48 -0400

    formatting

--- src/main.rs
+++ src/main.rs
@@ -89,6 +89,57 @@ impl Account {
 struct Database(Vec<Vec<Account>>);
 
 impl Database {
+    //  Build new database with preinitialized vectors
+    fn new() -> Self {
+        Self((0..((MAX - MIN) / PRIME) + 1).map(|_| Vec::new()).collect())
+    }
+
+    //  Determine if `user` and `pass` are legal strings and `user` doesn't aleady exist
+    async fn available_username(&self, user: &str, pass: &str) -> Result<Account, usize> {
+        check(user, pass).await?;
+
+        for account in self.0[user.to_string().hash()].iter() {
+            if account.user == user {
+                return Err(0);
+            }
+        }
+        Ok(Account::new(user.to_string(), pass.to_string()).await)
+    }
+
+    //  Push new specified account into vector of the hash value of `pass` if `available_username`
+    async fn add(&mut self, user: &str, pass: &str) -> Result<(), usize> {
+        match self.available_username(&user, &pass).await {
+            Ok(account) => Ok(self.0[account.user.hash()].push(account)),
+            Err(why) => Err(why),
+        }
+    }
+
+    //  Search method based on the hash value of the Account's `pass`
+    async fn find(&self, user: &str, pass: &str) -> Result<Option<&Account>, usize> {
+        check(user, pass).await?;
+
+        let row = &self.0[user.to_string().hash()];
+        if row.len() > 0 {
+            for account in row.iter() {
+                if user == account.user && pass == account.pass {
+                    return Ok(Some(account));
+                }
+            }
+        }
+        Ok(None)
+    }
+
+    //  Backup the database
+    async fn _backup(&self) -> Result<Result<(), sj_Error>, io_Error> {
+        Ok(to_writer(File::create("accounts.json")?, &self.0))
+    }
+
+    //  Set the database to the most recent backup
+    async fn _restore(&mut self) -> Result<(), io_Error> {
+        self.0 = from_reader(BufReader::new(File::open("accounts.json")?))?;
+        Ok(())
+    }
+
     #[cfg(feature = "testing")]
     //  Randomly generate and push `n` random accounts to the database
     async fn _gen_accounts(&mut self, amount: usize) {
@@ -128,64 +179,13 @@ impl Database {
             elapsed: b.duration_since(a).unwrap(),
         }
     }
-
-    //  Push new specified account into vector of the hash value of `pass` if `available_username`
-    async fn add(&mut self, user: &str, pass: &str) -> Result<(), usize> {
-        match self.available_username(&user, &pass).await {
-            Ok(account) => Ok(self.0[account.user.hash()].push(account)),
-            Err(why) => Err(why),
-        }
-    }
-
-    //  Determine if `user` and `pass` are legal strings and `user` doesn't aleady exist
-    async fn available_username(&self, user: &str, pass: &str) -> Result<Account, usize> {
-        check(user, pass).await?;
-
-        for account in self.0[user.to_string().hash()].iter() {
-            if account.user == user {
-                return Err(0);
-            }
-        }
-        Ok(Account::new(user.to_string(), pass.to_string()).await)
-    }
-
-    //  Search method based on the hash value of the Account's `pass`
-    async fn find(&self, user: &str, pass: &str) -> Result<Option<&Account>, usize> {
-        check(user, pass).await?;
-
-        let row = &self.0[user.to_string().hash()];
-        if row.len() > 0 {
-            for account in row.iter() {
-                if user == account.user && pass == account.pass {
-                    return Ok(Some(account));
-                }
-            }
-        }
-        Ok(None)
-    }
-
-    //  Backup the database
-    async fn _backup(&self) -> Result<Result<(), sj_Error>, io_Error> {
-        Ok(to_writer(File::create("accounts.json")?, &self.0))
-    }
-
-    //  Set the database to the most recent backup
-    async fn _restore(&mut self) -> Result<(), io_Error> {
-        self.0 = from_reader(BufReader::new(File::open("accounts.json")?))?;
-        Ok(())
-    }
-
-    //  Build new database with preinitialized vectors
-    fn new() -> Self {
-        Self((0..((MAX - MIN) / PRIME) + 1).map(|_| Vec::new()).collect())
-    }
 }
 
 #[tokio::main]
 async fn main() {
     let mut data = Database::new();
 
-    //  My first attempt at a `repl`
+    //  My attempt at a `repl`
     loop {
         if let Ok(string) = input().await {
             let cmd = string
@@ -195,17 +195,28 @@ async fn main() {
 
             match cmd.len() {
                 1 => match cmd[0] {
+                    "exit" | "quit" => break,
                     "total" => {
                         let mut total = 0;
                         for v in data.0.iter() {
                             total += v.len()
                         }
                         println!("{}", total);
-                        continue;
                     }
-                    "exit" | "quit" => break,
-                    "backup" => println!("{:?}", data._backup().await),
-                    "restore" => println!("{:?}", data._restore().await),
+                    "backup" => println!(
+                        "{}",
+                        match data._backup().await {
+                            Ok(_) => "0",
+                            Err(_) => "1",
+                        }
+                    ),
+                    "restore" => println!(
+                        "{}",
+                        match data._restore().await {
+                            Ok(_) => "0",
+                            Err(_) => "1",
+                        }
+                    ),
                     _ => (),
                 },
                 2 => match cmd[0] {
@@ -214,18 +225,23 @@ async fn main() {
                         "{}",
                         if let Ok(n) = cmd[1].parse() {
                             data._gen_accounts(n).await;
-                            "pass"
+                            "0"
                         } else {
-                            "fail"
+                            "1"
                         }
                     ),
                     _ => (),
                 },
                 3 => match cmd[0] {
                     "add" => {
-                        if let Err(n) = data.add(cmd[1], cmd[2]).await {
-                            println!("{}", ERRORS[n])
-                        }
+                        println!(
+                            "{}",
+                            if let Err(n) = data.add(cmd[1], cmd[2]).await {
+                                ERRORS[n]
+                            } else {
+                                "1"
+                            }
+                        )
                     }
                     "find" => match data.find(cmd[1], cmd[2]).await {
                         Ok(account) => println!("{:?}", account),
